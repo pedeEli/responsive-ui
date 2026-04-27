040f34400763ac92eab1d0d0140b5cb5d5de4c72
elias gerster <elias.gerster@outlook.de>
2026-04-27 17:16:23 +0200

    added build file

---
+++ build.js
@@ -0,0 +1,24 @@
+import fs from 'node:fs';
+import {fileURLToPath} from 'node:url';
+import {buildSync} from 'esbuild';
+
+const appDir = new URL('./', import.meta.url);
+const buildDir = new URL('./build/', import.meta.url);
+function resolveApp(path = '') {
+	return fileURLToPath(new URL(path, appDir));
+}
+function resolveBuild(path = '') {
+	return fileURLToPath(new URL(path, buildDir));
+}
+
+fs.copyFileSync(resolveApp('index.html'), resolveBuild('index.html'));
+fs.copyFileSync(resolveApp('index.css'), resolveBuild('index.css'));
+buildSync({
+	bundle: true,
+	format: 'esm',
+	logLevel: 'error',
+	entryPoints: [resolveApp('index.js')],
+	outdir: resolveBuild(),
+	legalComments: 'none',
+	minifyWhitespace: true
+});
